6876749af45c381f24ace7995497eee9b485b803
Richard Garrick <richg341@gmail.com>
2017-07-03 07:09:08 -0700

    add purchase popup box to site

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,14 @@
 // user interface logic
 $(document).ready(function() {
 
-  $("#buyTickets").click(function() {
-    console.log("Awesome!")
+  $('#buyTickets').click(function (event) {
+    event.preventDefault();
+    var popUp = $('#displaybox');
+    if(popUp.is(':hidden')){
+        popUp.fadeIn('slow');
+    }else{
+        popUp.fadeOut('slow');
+    }
   });
 
 
